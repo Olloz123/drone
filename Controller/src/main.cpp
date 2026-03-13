02d252ea3cd729f1a2ef5765f63fc404da2eb253
Tobias Olloz <olloz@student.chalmers.se>
2026-03-13 14:33:29 +0100

    #1 add minimal viable data sending example

--- Controller/src/main.cpp
+++ Controller/src/main.cpp
@@ -1,18 +1,49 @@
 #include <Arduino.h>
+#include <esp_now.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include "../../Communication/communication.hpp"
+
+rand_pack rand_data;
+movement_pack movement_data;
+
+uint8_t receiver_addr[] = {0x28, 0x05, 0xA5, 0x35, 0x7B, 0x58}; // example MAC
+
+void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Success" : "Failure");
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_send_cb(onDataSent);
+
+  esp_now_peer_info_t peerInfo = {};
+  memcpy(peerInfo.peer_addr, receiver_addr, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer");
+    return;
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  rand_data.x++;
+  rand_data.y++;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  esp_err_t result = esp_now_send(receiver_addr, (uint8_t *)&rand_data, sizeof(rand_data));
+
+  if (result != ESP_OK) {
+    Serial.println("Send error");
+  }
+
+  delay(1000);
 }
--- Drone/src/main.cpp
+++ Drone/src/main.cpp
@@ -1,18 +1,60 @@
 #include <Arduino.h>
+#include <esp_now.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include "../../Communication/communication.hpp"
+
+rand_pack rand_data;
+movement_pack movement_data;
+
+unsigned long prev_time = 0;
+
+void onRandDataReceive(const uint8_t *mac, const uint8_t *incomingData, int len) {
+    packet_type type = *(packet_type*)incomingData;
+
+    switch (type) {
+        case RAND_PACK:
+            if (len == sizeof(rand_pack)) {
+                memcpy(&rand_data, incomingData, sizeof(rand_data));
+            }
+            break;
+
+        case MOVEMENT_PACK:
+            if (len == sizeof(movement_pack)) {
+                memcpy(&movement_data, incomingData, sizeof(movement_data));
+            }
+            break;
+
+        default:
+            Serial.println("Unknown packet type");
+            break;
+    }
+}
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(200);
+
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error: Failed init esp_now");
+    return;
+  }
+
+  esp_now_register_recv_cb(onRandDataReceive);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  unsigned long now = millis();
+  if (now - prev_time >= 1000) {
+    prev_time = now;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    Serial.print("x = ");
+    Serial.println(rand_data.x);
+
+    Serial.print("y = ");
+    Serial.println(rand_data.y);
+  }
 }
